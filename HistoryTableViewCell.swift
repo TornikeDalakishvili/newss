2d6478f739f576bac3527a822124e9bf64d1a825
TornikeDalakishvili <dalaqishvili_tornike@mail.ru>
2017-07-21 13:06:50 +0400

    First Commit

---
+++ HistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryTableViewCell.swift
+//  NewsApp
+//
+//  Created by tornike dalaqishvili on 7/20/17.
+//  Copyright © 2017 tornike dalaqishvili. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  NewsApp
+//
+//  Created by tornike dalaqishvili on 7/18/17.
+//  Copyright © 2017 tornike dalaqishvili. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
